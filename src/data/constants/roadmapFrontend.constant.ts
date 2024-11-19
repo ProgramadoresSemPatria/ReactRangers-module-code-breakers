06da05f79c1cd9421b66f1c7a7e66ecf08bdb030
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-19 16:25:18 -0300

    refactor: remove comments

--- src/data/constants/roadmapFrontend.constant.ts
+++ src/data/constants/roadmapFrontend.constant.ts
@@ -234,43 +234,34 @@ const roadmapEdges = [
     { id: "e7-8", source: "7", target: "8", type: "block" },
     { id: "e8-9", source: "8", target: "9", type: "block" },
   
-    // Subtopic links for "Programming and Web Fundamentals" block
     { id: "e1-1-1", source: "1", target: "1-1", type: "subtopic" },
     { id: "e1-1-2", source: "1-1", target: "1-2", type: "subtopic" },
   
-    // Subtopic links for "Essential Front-End Technologies" block
     { id: "e2-1-1", source: "2", target: "2-1", type: "subtopic" },
     { id: "e2-1-2", source: "2-1", target: "2-2", type: "subtopic" },
     { id: "e2-2-3", source: "2-2", target: "2-3", type: "subtopic" },
   
-    // Subtopic links for "Professional Development and Code Organization" block
     { id: "e3-1-1", source: "3", target: "3-1", type: "subtopic" },
     { id: "e3-1-2", source: "3-1", target: "3-2", type: "subtopic" },
   
-    // Subtopic links for "Advanced JavaScript Development" block
     { id: "e4-1-1", source: "4", target: "4-1", type: "subtopic" },
     { id: "e4-1-2", source: "4-1", target: "4-2", type: "subtopic" },
     { id: "e4-2-3", source: "4-2", target: "4-3", type: "subtopic" },
     { id: "e4-3-4", source: "4-3", target: "4-4", type: "subtopic" },
   
-    // Subtopic links for "Frameworks and Modern Tools" block
     { id: "e5-1-1", source: "5", target: "5-1", type: "subtopic" },
     { id: "e5-1-2", source: "5-1", target: "5-2", type: "subtopic" },
     { id: "e5-2-3", source: "5-2", target: "5-3", type: "subtopic" },
   
-    // Subtopic links for "Responsive and Accessible Design" block
     { id: "e6-1-1", source: "6", target: "6-1", type: "subtopic" },
     { id: "e6-1-2", source: "6-1", target: "6-2", type: "subtopic" },
   
-    // Subtopic links for "Building and Deploying Projects" block
     { id: "e7-1-1", source: "7", target: "7-1", type: "subtopic" },
     { id: "e7-1-2", source: "7-1", target: "7-2", type: "subtopic" },
   
-    // Subtopic links for "Soft Skills and Market Preparation" block
     { id: "e8-1-1", source: "8", target: "8-1", type: "subtopic" },
     { id: "e8-1-2", source: "8-1", target: "8-2", type: "subtopic" },
   
-    // Subtopic links for "Mastery and Specialization" block
     { id: "e9-1-1", source: "9", target: "9-1", type: "subtopic" },
     { id: "e9-1-2", source: "9-1", target: "9-2", type: "subtopic" },
     { id: "e9-2-3", source: "9-2", target: "9-3", type: "subtopic" },

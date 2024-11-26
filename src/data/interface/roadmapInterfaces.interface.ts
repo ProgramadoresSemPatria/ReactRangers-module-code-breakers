5464f35c5536ce09276707fbb4277e87d761f55e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-26 14:05:50 -0300

    feat: create roadmap interfaces

---
+++ src/data/interface/roadmapInterfaces.interface.ts
@@ -0,0 +1,29 @@
+export interface Resource {
+  title: string;
+  url: string;
+}
+
+export interface Subtopic {
+  id: string;
+  title: string;
+  description?: string;
+  resources?: Resource[];
+}
+
+export interface Data extends Record<string, unknown> {
+    title: string;
+    description?: string;
+    subtopics: Subtopic[];
+  }
+
+export interface Position {
+  x: number;
+  y: number;
+}
+
+export interface RoadmapNodes {
+  id: string;
+  type: string;
+  position: Position;
+  data: Data;
+}

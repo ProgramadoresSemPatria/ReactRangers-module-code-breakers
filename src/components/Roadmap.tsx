877644ee827e3749ef2247dcd4c0437f404cffae
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-11-26 14:17:42 -0300

    fix: ensure correct mapping of nodes according to the interface

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -5,22 +5,23 @@ import '@xyflow/react/dist/style.css';
 import { roadmapNodes, roadmapEdges } from '@/data/constants/roadmapFrontend.constant';
 import { Merriweather } from "next/font/google";
 import MainTopicNode from './node-type/MainNode';
-import MainEdge from './edge-type/MainEdge';
-import TopicEdge from './edge-type/TopicEdge';
+import MainEdge from '@/data/hooks/useMainEdger';
+import TopicEdge from '@/data/hooks/useTopicEdger';
+import { Resource } from '@/data/interface';
 
 const merriweather = Merriweather({ subsets: ['latin'], weight: '700' });
 
 const nodeTypes = {
   main: MainTopicNode,
 };
-
 const edgeTypes = {
   main: MainEdge,
   subtopic: TopicEdge,
 };
 
 export default function RoadmapFlow() {
-  const [activeNodeResources, setActiveNodeResources] = useState<{ title: string; url: string }[]>([]);
+  const [activeNodeResources, setActiveNodeResources] = useState<Resource[]>([]);
+  
   const nodes = roadmapNodes.map((node) => ({
     ...node,
     style: {
@@ -35,14 +36,20 @@ export default function RoadmapFlow() {
 
   const onNodeClick = (event: React.MouseEvent, node: Node) => {
     if (node.type === "main") {
-      const relatedSubtopics = roadmapNodes.filter((subNode) => subNode.parentNode === node.id);
-      const resources = relatedSubtopics.flatMap((subNode) => subNode.data.resources || []);
+
+      const relatedSubtopics = roadmapNodes
+        .map((roadmapNode) => roadmapNode.data.subtopics)
+        .flat()
+        .filter((subNode) => subNode.id === node.id);
+
+      const resources = relatedSubtopics.flatMap((subNode) => subNode.resources || []);
+
       setActiveNodeResources(resources);
     }
   };
 
   return (
-    <div className={"h-screen bg-gray-50 p-4" + merriweather.className} >
+    <div className={"h-screen bg-gray-50 p-4" + merriweather.className}>
       <h1 className="text-2xl font-bold text-indigo-650 mb-4 text-center">
         Front-end Development Roadmap
       </h1>
@@ -59,6 +66,21 @@ export default function RoadmapFlow() {
           <Controls />
         </ReactFlow>
       </div>
+
+      {activeNodeResources.length > 0 && (
+        <div className="mt-4 p-4 bg-gray-200 rounded-md">
+          <h3 className="text-xl font-semibold mb-3">Active Node Resources</h3>
+          <ul>
+            {activeNodeResources.map((resource) => (
+              <li key={resource.title} className="mb-2">
+                <a href={resource.url} target="_blank" rel="noopener noreferrer" className="text-indigo-600">
+                  {resource.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }

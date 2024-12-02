5c7307976812dffbb099ce3ed1d4d7626e7165d4
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-12-02 14:43:17 -0300

    feat: send theme and toggle theme state as switch props

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,14 +1,17 @@
+'use client'
 import React from 'react'
 import Logo from './Logo'
 import { Switch } from '../ui/switch'
+import useTheme from '@/data/hooks/useTheme'
 
 export default function Header() {
+  const { theme, toggleTheme } = useTheme()
   return (
-    <header className="w-full flex justify-between px-10 h-24 items-center bg-gray-950 border-b-2 border-green-350">
+    <header className={`w-full flex justify-between px-10 h-24 items-center border-b-2  ${theme === "dark" ? ' bg-gray-950 text-white border-green-350 ' : 'border-indigo-650 bg-slate-50 '} `}>
       <Logo />
-
-      <Switch />
-
+      <abbr className='flex items-center justify-center' title={theme === 'dark'? 'Dark Mode: Active': 'Dark Mode: Disabled'}>
+        <Switch checked={theme === "dark"} onCheckedChange={toggleTheme} />
+      </abbr>
     </header>
   )
 }
